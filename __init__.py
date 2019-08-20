ceb150caedffc263a6d9bbb5b11344ba497ef01f
67981492 <kristambond@gmail.com>
2019-08-20 00:03:35 -0400

    remove unncessary files

--- __init__.py
+++
@@ -1,9 +0,0 @@
-"""A package to parse pupillary data."""
-
-from pupil_parse.config import plot_config, path_config
-# from pupil_parse.pupil_utils import preprocess_utils, summary_utils
-from pupil_parse.pupil_utils import * ## TODO: figure out how to import all the utils at once
-
-_ = plot_config()
-(raw_data_path, intermediate_data_path,
- processed_data_path, figure_path) = path_config()
